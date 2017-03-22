92b00d48103c7ad1761c3d41b7002710ba37c07e
adrtod <adrien.todeschini@gmail.com>
2017-03-22 15:23:36 +0100

    get paper titles in top_papers

--- top_papers.r
+++ top_papers.r
@@ -1,9 +1,29 @@
 # Find top 3 papers for each topic
 # Valerio Perrone
 
-gammas <- read.table("/homes/perrone/ctr/ICML2016/results50/final.gamma", sep = " ")
-top_papers <- apply(gammas,2,function(x) order(x,decreasing=TRUE)[1:3])
-files <- list.files("/homes/perrone/ctr/ICML2016/icml_pdf_conversion/icml2016/txt_files/",pattern =".txt")
-output <- apply(top_papers,2,function(x)   files[x]  )
+require(jsonlite)
 
-write.table(output,"/homes/perrone/ctr/ICML2016/results50/top_papers50.txt")
+# results_path = "/homes/perrone/ctr/ICML2016/results50/"
+# txt_files_path = "/homes/perrone/ctr/ICML2016/icml_pdf_conversion/icml2016/"
+
+results_path = "data/icml2016/lda_output"
+txt_files_path = "data/icml2016/papers_txt"
+
+data_path = "data/icml2016"
+
+nb_top = 3
+
+papers = fromJSON(file(file.path(data_path, "papers.json")))[, c("key", "title")]
+
+gammas <- read.table(file.path(results_path, "final.gamma"), sep=" ")
+
+top_papers <- apply(gammas, 2, function(x) order(x,decreasing=TRUE)[seq_len(nb_top)])
+
+files <- readLines(file.path(txt_files_path, "files.dat"))
+files <- tools::file_path_sans_ext(basename(files))
+
+titles = papers$title[match(papers$key, files)]
+
+output <- apply(top_papers, 2, function(x) titles[x])
+
+write.table(output, file.path(results_path, "top_papers50.txt"))
