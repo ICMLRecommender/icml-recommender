141f130272f01a3ace4af450a514bc8d75a99009
adrtod <adrien.todeschini@gmail.com>
2017-04-24 12:46:01 +0200

    fix reading supp_pdf

--- scrape_icml.r
+++ scrape_icml.r
@@ -82,9 +82,11 @@ parse_paper = function(url) {
   if (dl_pdf && !file.exists(fn))
     download.file(out$pdf_url, fn)
   
-  fn = file.path(data_path, "papers", basename(out$supp_pdf_url))
-  if (dl_supp_pdf && nchar(out$supp_pdf_url)>0 && !file.exists(fn))
-    download.file(out$supp_pdf_url, file.path(data_path, "papers", out$supp_pdf_url))
+  if (nchar(out$supp_pdf_url)>0) {
+    fn = file.path(data_path, "papers", basename(out$supp_pdf_url))
+    if (dl_supp_pdf && !file.exists(fn))
+      download.file(out$supp_pdf_url, file.path(data_path, "papers", out$supp_pdf_url))
+  }
   
   return(tbl_df(out))
 }
